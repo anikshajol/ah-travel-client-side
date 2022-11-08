678b6200c8f9b82a4b9b414387c4e98cc2f34b50
anikshajol <anikshajol@gmail.com>
2022-11-08 15:50:59 +0600

    components routes update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import { routes } from "./router/routes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="">
+      <RouterProvider router={routes}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Router/Routes.js
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Components/Home/Home";
+import Login from "../Components/Login/Login";
+import Main from "../Layouts/Main";
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
